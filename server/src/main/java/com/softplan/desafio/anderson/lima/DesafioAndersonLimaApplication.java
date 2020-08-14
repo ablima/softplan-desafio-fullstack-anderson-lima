2828e98c1317270b8d0306734cba3aaa6e38b35f
Anderson Barbosa Lima <anderson.l@sidiamao.com.br>
2020-08-14 16:12:16 -0400

    Added base project for backend and frontend
    Signed-off-by: Anderson Barbosa Lima <anderson.l@sidiamao.com.br>

---
+++ server/src/main/java/com/softplan/desafio/anderson/lima/DesafioAndersonLimaApplication.java
@@ -0,0 +1,13 @@
+package com.softplan.desafio.anderson.lima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAndersonLimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioAndersonLimaApplication.class, args);
+	}
+
+}
